76cc8ed721e4027af6854f14caab9e7ddb7768d7
Natalya <m_d.r.lecter@protonmail.com>
2024-05-03 23:49:19 +0400

    js wip

--- js/script.js
+++ js/script.js
@@ -23,16 +23,56 @@ const toppingsToggle = () => {
 const getPizzas = async () => {
     try {
         const response = await fetch('http://localhost:3000/api/products');
-        console.log('response: ', response);
+        if(!response.ok) {
+            throw new Error('fetching server problems')
+        }
+        return  await response.json();
     } catch (error) {
         console.error(`Error fetching pizzas: ${error}`);
     }
 
 }
 
-const renderPizzas = () => {
-    const pizzas = getPizzas();
-    console.log('getPizzas: ', getPizzas);
+const createCard = (data) => {
+    console.log('data: ', data);
+    const card = document.createElement('article');
+    card.classList.add('card');
+    card.innerHTML = `
+        <picture>
+            <source srcset="${data.images[1]}" type="image/webp">
+            <img class="card__image" src="${data.images[0]}" alt="${data.name.en}">
+        </picture>
+        <div class="card__content">
+            <h3 class="card__title">${data.name.en}</h3>
+            <p class="card__info">
+                <span class="card__price">${data.price['30cm']}$</span>
+                <span>/</span>
+                <span class="card__weight">25 cm</span>
+            </p>
+            <button class="card__button">Choose</button>
+        </div>
+    `;
+
+    return card;
+}
+
+const renderPizzas = async () => {
+    const pizzas = await getPizzas();
+    const pizzasList = document.querySelector('.pizza__list');
+    pizzasList.textContent = '';
+
+    const items = pizzas.map( data => {
+        const item = document.createElement('li');
+        item.classList.add('pizza__item');
+        item.textContent = data.name.en;
+
+        const card = createCard(data);
+        item.append(card);
+
+        return item;
+    });
+
+    pizzasList.append(...items);
 };
 
 const init = () => {
