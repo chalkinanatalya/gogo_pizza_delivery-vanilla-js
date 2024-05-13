bd0d1f1b9c249cf9b020f6b63967672952188042
Natalya <m_d.r.lecter@protonmail.com>
2024-05-13 21:54:29 +0400

    toppings search

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 import { renderPizzas } from "./modules/renderPizzas.js";
+import { renderToppings } from "./modules/renderToppings.js";
 import { toppingsToggle } from "./modules/toppingToggle.js";
 
 
 const init = () => {
     toppingsToggle();
+    renderToppings();
     renderPizzas();
 }
 
